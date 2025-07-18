e047f058a6de166f1fb3c841032b3c42bf8c012e
Attszab <szabo.attila.informatika@gmail.com>
2025-07-18 13:07:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -21,4 +21,7 @@ def convert():
     return send_file(output_path, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run()
+    import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+
